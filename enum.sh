12730ee536f7202d008a300f455e27c18ff741a6
Tyler <tc.anderson.tech@gmail.com>
2025-11-28 14:36:37 -0500

    enum.sh fixing line formatting

--- enum.sh
+++ enum.sh
@@ -2,8 +2,13 @@
 
 url=$1
 
-if [ ! -d "enum_$url" ];then mkdir enum_$url fi
-if [ ! -d "enum_$url/recon" ];then mkdir enum_$url/recon fi
+if [ ! -d "enum_$url" ];then 
+	mkdir enum_$url
+fi
+
+if [ ! -d "enum_$url/recon" ];then
+	mkdir enum_$url/recon
+fi
 
 echo "[+] assetfinder - subdomain recon step..."
 assetfinder $url >> enum_$url/recon/subdomains.txt
